3e62ca4627da037ca7d39907fabf6e3b73e74bff
deathamongstlife <r79767525@gmail.com>
2025-05-29 05:31:33 -0400

    Update tag.tsx

--- src/patches/tag.tsx
+++ src/patches/tag.tsx
@@ -2,12 +2,12 @@ import { findByProps } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
 import { findInReactTree } from "@vendetta/utils";
 
-const Tag = findByProps("getBotLabel")
+const Tag = findByProps("getBotLabel");
 
 export default () => after("default", Tag, ([{ text, textColor, backgroundColor }], ret) => {
-    const label = findInReactTree(ret, (c) => typeof c.props.children === "string")
+    const label = findInReactTree(ret, (c) => typeof c.props.children === "string");
 
-    if (text) label.props.children = text
-    if (textColor) label.props.style.push({ color: textColor })
-    if (backgroundColor) ret.props.style.push({ backgroundColor })
-})
+    if (text) label.props.children = text;
+    if (textColor) label.props.style.push({ color: textColor });
+    if (backgroundColor) ret.props.style.push({ backgroundColor });
+});
