2bc3cb34c9eb47c2eedec95cc007d5c36c048171
Scarlet Jean <r79767525@gmail.com>
2025-05-29 13:06:58 -0400

    Enabled last ckmmit  testing member list

--- src/patches/sidebar.tsx
+++ src/patches/sidebar.tsx
@@ -1,45 +1,53 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
-import { findInReactTree } from "@vendetta/utils";
-import getTag, { BUILT_IN_TAGS } from "../lib/getTag";
+import getTag from "../lib/getTag";
 
 const Rows = findByProps("GuildMemberRow");
 const TagModule = findByProps("getBotLabel");
-const getBotLabel = TagModule?.getBotLabel;
 const GuildStore = findByStoreName("GuildStore");
 
 export default () => {
-    if (!Rows?.GuildMemberRow) {
-        return () => {};
-    }
+    const patches = [];
+    
+    // Try GuildMemberRow
+    if (Rows?.GuildMemberRow) {
+        patches.push(after("type", Rows.GuildMemberRow, ([{ guildId, channel, user }], ret) => {
+            try {
+                if (!ret || !user) return ret;
+                
+                const guild = GuildStore?.getGuild?.(guildId);
+                const tag = getTag(guild, channel, user);
 
-    return after("type", Rows.GuildMemberRow, ([{ guildId, channel, user }], ret) => {
-        try {
-            if (!ret || !user) return ret;
-            
-            const guild = GuildStore?.getGuild?.(guildId);
-            const tag = getTag(guild, channel, user);
-
-            if (tag && TagModule?.default) {
-                // Simple approach: just try to add to ret.props.children
-                if (ret.props && ret.props.children) {
+                if (tag && TagModule?.default && ret.props?.children) {
+                    // Try to add tag with [SIDEBAR] marker
+                    const tagElement = React.createElement(TagModule.default, {
+                        type: 0,
+                        text: tag.text + "[SIDEBAR]", // Marker to see if it worked
+                        textColor: tag.textColor,
+                        backgroundColor: tag.backgroundColor,
+                        verified: tag.verified
+                    });
+                    
                     if (Array.isArray(ret.props.children)) {
-                        ret.props.children.push(
-                            React.createElement(TagModule.default, {
-                                type: 0,
-                                text: tag.text,
-                                textColor: tag.textColor,
-                                backgroundColor: tag.backgroundColor,
-                                verified: tag.verified
-                            })
-                        );
+                        ret.props.children.push(tagElement);
+                    } else {
+                        ret.props.children = [ret.props.children, tagElement];
                     }
                 }
+            } catch (error) {
+                // Silent
             }
-        } catch (error) {
-            // Silent
-        }
-        
-        return ret;
-    });
+            return ret;
+        }));
+    }
+
+    return () => {
+        patches.forEach(unpatch => {
+            try {
+                unpatch?.();
+            } catch (error) {
+                // Silent
+            }
+        });
+    };
 };
