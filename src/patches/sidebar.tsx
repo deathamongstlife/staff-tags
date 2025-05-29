4e617bedc8d44361253b27b0be618a489079b12d
Scarlet Jean <r79767525@gmail.com>
2025-05-29 13:13:33 -0400

    Pot Fixes

--- src/patches/sidebar.tsx
+++ src/patches/sidebar.tsx
@@ -1,44 +1,67 @@
-import { findByProps, findByStoreName } from "@vendetta/metro";
+import { findByProps, findByStoreName, findByName } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
 import getTag from "../lib/getTag";
 
-const Rows = findByProps("GuildMemberRow");
 const TagModule = findByProps("getBotLabel");
 const GuildStore = findByStoreName("GuildStore");
+
+// Try to find different possible components
+const MemberListItem = findByName("MemberListItem", false);
+const VoiceUser = findByName("VoiceUser", false);
+const Member = findByName("Member", false);
+const ConnectedMember = findByName("ConnectedMember", false);
 
 export default () => {
     const patches = [];
     
-    // Try GuildMemberRow
-    if (Rows?.GuildMemberRow) {
-        patches.push(after("type", Rows.GuildMemberRow, ([{ guildId, channel, user }], ret) => {
-            try {
-                if (!ret || !user) return ret;
-                
-                const guild = GuildStore?.getGuild?.(guildId);
-                const tag = getTag(guild, channel, user);
+    const addTag = (componentName) => ([props], ret) => {
+        try {
+            const { guildId, channel, user } = props || {};
+            
+            if (!ret || !user) return ret;
+            
+            const guild = GuildStore?.getGuild?.(guildId);
+            const tag = getTag(guild, channel, user);
 
-                if (tag && TagModule?.default && ret.props?.children) {
-                    // Try to add tag with [SIDEBAR] marker
-                    const tagElement = React.createElement(TagModule.default, {
-                        type: 0,
-                        text: tag.text + "[SIDEBAR]", // Marker to see if it worked
-                        textColor: tag.textColor,
-                        backgroundColor: tag.backgroundColor,
-                        verified: tag.verified
-                    });
-                    
+            if (tag && TagModule?.default) {
+                const tagElement = React.createElement(TagModule.default, {
+                    type: 0,
+                    text: tag.text + `[${componentName}]`, // Visual marker
+                    textColor: tag.textColor,
+                    backgroundColor: tag.backgroundColor,
+                    verified: tag.verified
+                });
+                
+                // Try to add to children
+                if (ret.props?.children) {
                     if (Array.isArray(ret.props.children)) {
                         ret.props.children.push(tagElement);
                     } else {
                         ret.props.children = [ret.props.children, tagElement];
                     }
                 }
-            } catch (error) {
-                // Silent
             }
-            return ret;
-        }));
+        } catch (error) {
+            // Silent
+        }
+        return ret;
+    };
+
+    // Try each component
+    if (MemberListItem) {
+        patches.push(after("default", MemberListItem, addTag("MemberListItem")));
+    }
+    
+    if (VoiceUser) {
+        patches.push(after("default", VoiceUser, addTag("VoiceUser")));
+    }
+    
+    if (Member) {
+        patches.push(after("default", Member, addTag("Member")));
+    }
+    
+    if (ConnectedMember) {
+        patches.push(after("default", ConnectedMember, addTag("ConnectedMember")));
     }
 
     return () => {
