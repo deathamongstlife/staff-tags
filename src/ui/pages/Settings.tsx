72bf6cb8560c538980b434a86475bca7772a6501
deathamongstlife <r79767525@gmail.com>
2025-05-29 05:30:30 -0400

    Update Settings.tsx

--- src/ui/pages/Settings.tsx
+++ src/ui/pages/Settings.tsx
@@ -2,13 +2,13 @@ import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { Forms, General } from "@vendetta/ui/components";
 
-const { ScrollView } = General
-const { FormSection, FormSwitchRow } = Forms
+const { ScrollView } = General;
+const { FormSection, FormSwitchRow } = Forms;
 
 export default function Settings() {
-    useProxy(storage)
+    useProxy(storage);
 
-    return <ScrollView style={{ flex: 1 }} >
+    return <ScrollView style={{ flex: 1 }}>
         <FormSection title="Tag style">
             <FormSwitchRow
                 label="Use top role color for tag backgrounds"
@@ -18,5 +18,5 @@ export default function Settings() {
                 }}
             />
         </FormSection>
-    </ScrollView>
+    </ScrollView>;
 }
