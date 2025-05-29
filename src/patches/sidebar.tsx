e5d773735c0d3d6afef618409e9c57c86c86d44e
Scarlet Jean <r79767525@gmail.com>
2025-05-29 13:34:18 -0400

    Potentially Fixrd

--- src/patches/sidebar.tsx
+++ src/patches/sidebar.tsx
@@ -1,76 +1,41 @@
-import { findByProps, findByStoreName, findByName } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
+import { findByName, findByStoreName } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
 import getTag from "../lib/getTag";
 
-const TagModule = findByProps("getBotLabel");
+const RowManager = findByName("RowManager");
 const GuildStore = findByStoreName("GuildStore");
 
-// Try to find different possible components
-const MemberListItem = findByName("MemberListItem", false);
-const VoiceUser = findByName("VoiceUser", false);
-const Member = findByName("Member", false);
-const ConnectedMember = findByName("ConnectedMember", false);
-
 export default () => {
-    const patches = [];
-    
-    const addTag = (componentName) => ([props], ret) => {
-        try {
-            const { guildId, channel, user } = props || {};
-            
-            if (!ret || !user) return ret;
-            
-            const guild = GuildStore?.getGuild?.(guildId);
-            const tag = getTag(guild, channel, user);
+    if (!RowManager) {
+        return () => {};
+    }
 
-            if (tag && TagModule?.default) {
-                const tagElement = React.createElement(TagModule.default, {
-                    type: 0,
-                    text: tag.text + `[${componentName}]`, // Visual marker
-                    textColor: tag.textColor,
-                    backgroundColor: tag.backgroundColor,
-                    verified: tag.verified
-                });
+    return before("generate", RowManager.prototype, ([data]) => {
+        try {
+            // Check if this is a member list row with user data
+            if (data?.message?.author || data?.user) {
+                const user = data.message?.author || data.user;
+                const guildId = data.guildId || data.message?.guild_id;
                 
-                // Try to add to children
-                if (ret.props?.children) {
-                    if (Array.isArray(ret.props.children)) {
-                        ret.props.children.push(tagElement);
-                    } else {
-                        ret.props.children = [ret.props.children, tagElement];
+                if (user && guildId) {
+                    const guild = GuildStore?.getGuild?.(guildId);
+                    const tag = getTag(guild, null, user);
+                    
+                    if (tag) {
+                        // Modify the data before it gets rendered
+                        if (data.text) {
+                            data.text = data.text + ` ${tag.text}`;
+                        }
+                        
+                        // Try to add role styling
+                        if (tag.backgroundColor && data.roleStyle !== undefined) {
+                            data.roleStyle = tag.backgroundColor;
+                        }
                     }
                 }
             }
         } catch (error) {
             // Silent
         }
-        return ret;
-    };
-
-    // Try each component
-    if (MemberListItem) {
-        patches.push(after("default", MemberListItem, addTag("MemberListItem")));
-    }
-    
-    if (VoiceUser) {
-        patches.push(after("default", VoiceUser, addTag("VoiceUser")));
-    }
-    
-    if (Member) {
-        patches.push(after("default", Member, addTag("Member")));
-    }
-    
-    if (ConnectedMember) {
-        patches.push(after("default", ConnectedMember, addTag("ConnectedMember")));
-    }
-
-    return () => {
-        patches.forEach(unpatch => {
-            try {
-                unpatch?.();
-            } catch (error) {
-                // Silent
-            }
-        });
-    };
+    });
 };
