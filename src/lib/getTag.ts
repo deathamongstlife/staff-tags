3a8299e9e701b5c5a725125d8c8473cd78040ab0
deathamongstlife <r79767525@gmail.com>
2025-05-29 05:32:27 -0400

    Update getTag.ts

--- src/lib/getTag.ts
+++ src/lib/getTag.ts
@@ -4,10 +4,10 @@ import { storage } from "@vendetta/plugin";
 import { rawColors } from "@vendetta/ui";
 
 // Permissions
-const { Permissions } = constants
-const { computePermissions } = findByProps("computePermissions", "canEveryoneRole")
+const { Permissions } = constants;
+const { computePermissions } = findByProps("computePermissions", "canEveryoneRole");
 
-const GuildMemberStore = findByStoreName("GuildMemberStore")
+const GuildMemberStore = findByStoreName("GuildMemberStore");
 
 export const BUILT_IN_TAGS = [
     i18n.Messages.AI_TAG,
@@ -16,15 +16,15 @@ export const BUILT_IN_TAGS = [
     i18n.Messages.SYSTEM_DM_TAG_SYSTEM,
     i18n.Messages.GUILD_AUTOMOD_USER_BADGE_TEXT,
     i18n.Messages.REMIXING_TAG
-]
+];
 
 interface Tag {
-    text: string
-    textColor?: any
-    backgroundColor?: any
-    verified?: boolean | ((guild, channel, user) => boolean)
-    condition?: (guild, channel, user) => boolean
-    permissions?: string[]
+    text: string;
+    textColor?: any;
+    backgroundColor?: any;
+    verified?: boolean | ((guild, channel, user) => boolean);
+    condition?: (guild, channel, user) => boolean;
+    permissions?: string[];
 }
 
 const tags: Tag[] = [
@@ -58,29 +58,29 @@ const tags: Tag[] = [
         backgroundColor: rawColors.BLUE_345,
         permissions: ["MANAGE_MESSAGES", "KICK_MEMBERS", "BAN_MEMBERS"]
     }
-]
+];
 
 export default function getTag(guild, channel, user) {
-    let permissions
+    let permissions;
     if (guild) {
         const permissionsInt = computePermissions({
             user: user,
             context: guild,
             overwrites: channel?.permissionOverwrites
-        })
+        });
         permissions = Object.entries(Permissions)
             .map(([permission, permissionInt]: [string, bigint]) =>
                 permissionsInt & permissionInt ? permission : "")
-            .filter(Boolean)
+            .filter(Boolean);
     }
 
     for (const tag of tags) {
         if (tag.condition?.(guild, channel, user) ||
             tag.permissions?.some(perm => permissions?.includes(perm))) {
 
-            let roleColor = storage.useRoleColor ? GuildMemberStore.getMember(guild?.id, user.id)?.colorString : undefined
-            let backgroundColor = roleColor ? roleColor : tag.backgroundColor ?? rawColors.BRAND_500
-            let textColor = (roleColor || !tag.textColor) ? (chroma(backgroundColor).get('lab.l') < 70 ? rawColors.WHITE_500 : rawColors.BLACK_500) : tag.textColor
+            let roleColor = storage.useRoleColor ? GuildMemberStore.getMember(guild?.id, user.id)?.colorString : undefined;
+            let backgroundColor = roleColor ? roleColor : tag.backgroundColor ?? rawColors.BRAND_500;
+            let textColor = (roleColor || !tag.textColor) ? (chroma(backgroundColor).get('lab.l') < 70 ? rawColors.WHITE_500 : rawColors.BLACK_500) : tag.textColor;
 
             return {
                 ...tag,
@@ -89,7 +89,7 @@ export default function getTag(guild, channel, user) {
                 verified: typeof tag.verified === "function" ? tag.verified(guild, channel, user) : tag.verified ?? false,
                 condition: undefined,
                 permissions: undefined
-            }
+            };
         }
     }
 }
